7ccc9bc8e9159321f209a587fdd91a3e64bf0fb2
Rohan Asokan <rohan.asokan@students.iiit.ac.in>
2021-09-22 10:13:25 +0400

    Update gitignore files

--- compile_script.sh
+++
@@ -1,9 +0,0 @@
-# Compile all the required files
-i686-elf-gcc -std=gnu99 -ffreestanding -g -c start.s -o start.o
-i686-elf-gcc -std=gnu99 -ffreestanding -g -c kernel.c -o kernel.o
-
-# Link the files into a final executable
-i686-elf-gcc -ffreestanding -nostdlib -g -T linker.ld start.o kernel.o -o agos.elf -lgcc
-
-# Run the kernel
-qemu-system-i386 -machine type=pc-i440fx-3.1 -kernel agos.elf
